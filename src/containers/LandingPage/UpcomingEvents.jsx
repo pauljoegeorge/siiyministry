769c1346132bffc6af617571e98a09678bd6d50e
pauljoegeorge <pauljoegeorge@gmail.com>
2023-05-30 19:53:05 +0900

    move upcoming events to flex

--- src/containers/LandingPage/UpcomingEvents.jsx
+++ src/containers/LandingPage/UpcomingEvents.jsx
@@ -1,6 +1,15 @@
 import React, { useEffect } from 'react';
+import styled from 'styled-components';
 import { Heading } from './components/Components';
 import { useData } from '../../hooks/useData';
+
+const EventWrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  @media (max-width: 600px) {
+    flex-wrap: wrap;
+  }
+`;
 
 const UpcomingEvents = () => {
   const { upcomingEvents, actions } = useData();
@@ -15,30 +24,7 @@ const UpcomingEvents = () => {
         <Heading className="text-4xl font font-extrabold tracking-tight text-gray-900 sm:text-6xl pb-10">
           {upcomingEvents?.head}
         </Heading>
-
-        {/* <div className="grid grid-cols-2 gap-y-10 sm:grid-cols-2 gap-x-12 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
-          {page.events.map((event) => (
-            <a key={event.id} href={`/event/${event.uid}`} className="group">
-              <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                <img
-                  src={require(`../../assets/data/LandingPage/UpcomingEvents/${event.icon}`)}
-                  alt={event.alt}
-                  className="w-full h-80 object-center object-cover group-hover:opacity-75"
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-1xl text-gray-700">
-                    <p>{event.title}</p>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{event.datetime} </p>
-                </div>
-              </div>
-            </a>
-          ))}
-        </div> */}
-        <div className="grid sm:grid-cols-4 lg:grid-cols-4 gap-x-8 py-8">
-          <div />
+        <EventWrapper>
           {upcomingEvents?.events?.map((event) => (
             <a key={event?.id} href={`/event/${event?.uid}`} className="group">
               <div className="mt-10 w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
@@ -61,7 +47,7 @@ const UpcomingEvents = () => {
               </div>
             </a>
           ))}
-        </div>
+        </EventWrapper>
       </div>
       <div className="hero-content text-center" width="80%">
         <div>
